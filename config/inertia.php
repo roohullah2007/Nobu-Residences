9c4ba1449ac6768e51858e89dec80fe0d8260188
macbook air <macbookair@macbooks-MacBook-Air.local>
2026-01-03 01:49:32 +0500

    Disable SSR temporarily for debugging

---
+++ config/inertia.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Server Side Rendering
+    |--------------------------------------------------------------------------
+    |
+    | These options configures if and how Inertia uses Server Side Rendering
+    | to pre-render the initial visits made to your application's pages.
+    |
+    | You can disable SSR by setting 'enabled' to false.
+    |
+    */
+
+    'ssr' => [
+        'enabled' => false,  // Disabled temporarily for debugging
+        'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Testing
+    |--------------------------------------------------------------------------
+    |
+    | The values described here are used to locate Inertia components on the
+    | filesystem. For instance, when using `assertInertia`, the assertion
+    | attempts to locate the component as a file relative to any of the
+    | paths AND with any of the extensions specified here.
+    |
+    */
+
+    'testing' => [
+        'ensure_pages_exist' => true,
+        'page_paths' => [
+            resource_path('js/Pages'),
+            resource_path('js/Website/Pages'),
+        ],
+        'page_extensions' => [
+            'js',
+            'jsx',
+            'svelte',
+            'ts',
+            'tsx',
+            'vue',
+        ],
+    ],
+
+];
